4c015e3bd0843ec829d37216a0b5e23e8367e365
Adjei-stein <adjeisteiner@gmail.com>
2024-02-02 16:45:31 +0000

    Fixed the toggle for dark and light theme

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
+
     const body = document.querySelector("body"),
-        sideNav = body.querySelector(".side-nav"),
-        toggle = body.querySelector(".toggle-area"),
-        themeSwitch = body.querySelector(".toggle-switch"),
-        toggleSwitchButton = body.querySelector(".toggle-switch-button");
+        themeSwitch = body.querySelector("#toggle-switch");
 
     themeSwitch.addEventListener("click", () => {
+
         body.classList.toggle("dark");
-        console.log(body, sideNav, toggle, themeSwitch, toggleSwitchButton);
+        themeSwitch.classList.toggle("active");
+        
+
     });
-});
+
